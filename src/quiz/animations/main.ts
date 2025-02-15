f0c210f3834ae183d5ae7136a13e7a6e82d9192e
Rachana Kallada Jayaraj <132250274+rachkj@users.noreply.github.com>
2025-02-14 23:43:16 -0500

    Update main.ts

--- src/quiz/animations/main.ts
+++ src/quiz/animations/main.ts
@@ -13,36 +13,18 @@ const alice10 = document.querySelector<HTMLElement>("#alice1");
 const alice20 = document.querySelector<HTMLElement>("#alice2");
 const alice30 = document.querySelector<HTMLElement>("#alice3");
 
-if(alice10 && alice20 && alice30) {
-  // Promise chain  
-  alice10.animate(aliceTumbling1, aliceTiming1).finished  
-    .then(() => {
-        return alice20
-                .animate(aliceTumbling1, aliceTiming1)
-                .finished;     
-    })
-    .then(() => {
-      return alice30
-              .animate(aliceTumbling1, aliceTiming1)
-              .finished;
-    })
-    .catch((err) => alert(`Error when promising ... ${err.message}`));
-}
-else{
+if (alice10 && alice20 && alice30) {
+  // Simplified using async/await instead of the promise chain
+  (async () => {
+    try {
+
+      await alice10.animate(aliceTumbling1, aliceTiming1).finished;
+      await alice20.animate(aliceTumbling1, aliceTiming1).finished;
+      await alice30.animate(aliceTumbling1, aliceTiming1).finished;
+    } catch (err: any) {
+      alert(`Error when promising ... ${err.message}`);
+    }
+  })();
+} else {
   console.warn("#alice not found");
 }
-
-// alice10
-//     .animate(aliceTumbling1, aliceTiming1)
-//     .finished
-//     .then((res) => {
-//         console.log(res);
-//         alice20
-//             .animate(aliceTumbling1, aliceTiming1)
-//             .finished
-//             .then((res) => {
-//                 console.log(res);
-//                 alice30.animate(aliceTumbling1, aliceTiming1);
-//             })
-//     });
-  
